35c27c06b47c2a96071a223aeeb4c305a6a8c316
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-10-06 10:09:45 -0400

    delete put route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -85,13 +85,6 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-router.put('/upvote', withAuth, async (req, res) => {
-  await Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      res.status(500).json(err);
-    });
-});
 
 router.put('/:id', withAuth, async (req, res) => {
   await Post.update(
